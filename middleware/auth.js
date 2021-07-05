b20332e433ad28ddd9c44719a7851546c1ebfe77
vedant1723 <vedant.pruthi1723@gmail.com>
2021-07-05 19:33:29 +0530

    Middleware Added

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtokem");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.header("x-api-key");
+
+    if (!token) {
+      return res.json({
+        statusCode: 400,
+        message: "No Token, Authorization Denied!",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.jwtSecret);
+    req.emp = decoded.emp;
+    next();
+  } catch (error) {
+    return res.json({ statusCode: 400, message: "Token is invalid!" });
+  }
+};
