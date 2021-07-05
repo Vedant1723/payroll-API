b53c2ef3d1ec1a5062714867cd66a491f23fbabc
shubham anand <shubhamanand190@gmail.com>
2021-07-05 16:11:27 +0530

    Models Updated

---
+++ models/Attendance.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const AttendanceSchema = new mongoose.Schema({
+  markAs: {
+    type: String,
+  },
+  reason: {
+    type: String,
+    default: "",
+  },
+  employeeID: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  businessID: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = Attendance = mongoose.model("Attendance", AttendanceSchema);
