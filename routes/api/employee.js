0dc34c88eb53d304f99a64520e8fefbb7e133b14
Chirag Sharma <sharma.chirag2669@gmail.com>
2021-07-05 17:36:19 +0530

    Routes updated

--- routes/api/employee.js
+++ routes/api/employee.js
@@ -3,16 +3,20 @@ const router = require('express').Router();
 
 //@GET route
 //@DESC GET Employee
-router.get('/getemployee', employeeController.get);
+router.get('/get-employee', employeeController.get);
 
 //@POST route
 //@DESC CREATE Employee
-router.get('/createemployee', employeeController.create);
-//@GET route
-//@DESC GET Employee
-router.get('/getemployee', employeeController.get);
-//@GET route
-//@DESC GET Employee
-router.get('/getemployee', employeeController.get);
+router.post('/create-employee', employeeController.create);
+
+//@PUT route
+//@DESC UPDATE Employee
+
+router.put('/update-employee/:id', employeeController.update);
+
+//@DELETE route
+//@DESC DELETE Employee
+router.delete('/delete-employee/:id', employeeController.delete);
+
 // /*-----------------***Routes exported**-----------------------*/
 module.exports = router;
