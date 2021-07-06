a7554c683f388abc956e09d5ef82b5437e8f20d1
vedant1723 <vedant.pruthi1723@gmail.com>
2021-07-06 15:34:36 +0530

    Employee routes Updated

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,12 +1,14 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Employee = require('../models/Employee');
-require('dotenv').config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Employee = require("../models/Employee");
+require("dotenv").config();
 
 //@GET Route
 //@DESC Get Employee
 exports.get = async (req, res) => {
   try {
+    const emp = await Employee.find({ businessID: req.emp.id });
+    res.json(emp);
   } catch (error) {
     console.log(error.message);
   }
@@ -15,7 +17,22 @@ exports.get = async (req, res) => {
 //@POST Route
 //@DESC Create Employee
 exports.create = async (req, res) => {
+  const { name, email, phone, address, date, salary, salaryType } = req.body;
   try {
+    var empObj = {
+      name: name,
+      email: email,
+      phone: phone,
+      address: address,
+      businessID: req.emp.id,
+      date: date,
+      salary: salary,
+      salaryType: salaryType,
+    };
+    const emp = new Employee(empObj);
+
+    await emp.save();
+    res.json({ statusCode: 200, message: "Employee Created", data: emp });
   } catch (error) {
     console.log(error.message);
   }
@@ -25,8 +42,16 @@ exports.create = async (req, res) => {
 //@DESC Update Employee
 exports.update = async (req, res) => {
   try {
+    const id = req.params.id;
+    const update = req.body;
+    const options = { new: true };
+    const result = await Employee.findByIdAndUpdate(id, update, options);
+    res.json({ statusCode: 200, message: "Successful", data: result });
   } catch (error) {
     console.log(error.message);
+    if (error.kind == "ObjectId") {
+      return res.json({ statusCode: 400, message: "ID is Invalid" });
+    }
   }
 };
 
@@ -34,6 +59,9 @@ exports.update = async (req, res) => {
 //@DESC Delete Employee
 exports.delete = async (req, res) => {
   try {
+    const id = req.params.id;
+    const result = await Employee.findByIdAndDelete(id);
+    res.json({ statusCode: 200, message: "Successful", data: result });
   } catch (error) {
     console.log(error.message);
   }
